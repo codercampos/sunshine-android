d20684bdc0121b35310f767dea4303e60c1d76a7
Carlos Campos <ccampos@xinobistudio.com>
2017-01-02 17:55:52 -0600

    first commit

---
+++ app/src/main/java/com/xinobistudio/sunshine/MainActivity.java
@@ -0,0 +1,41 @@
+package com.xinobistudio.sunshine;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.activity_main, new PlaceHolderFragment())
+                    .commit();
+        }
+    }
+
+    public static class PlaceHolderFragment extends Fragment{
+        public PlaceHolderFragment(){
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            return rootView;
+        }
+    }
+}
+
+
