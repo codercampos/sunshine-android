176cda120fe88d893cfcdff6c0c3c397689e776b
Carlos Campos <ccampos@xinobistudio.com>
2017-01-09 13:01:53 -0600

    Added task to get weather information

--- app/src/main/java/com/xinobistudio/sunshine/MainActivity.java
+++ app/src/main/java/com/xinobistudio/sunshine/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,6 +14,12 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,36 +31,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if(savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.activity_main, new PlaceHolderFragment())
+                    .add(R.id.activity_main, new ForecastFragment())
                     .commit();
-        }
-    }
-
-    public static class PlaceHolderFragment extends Fragment{
-        public PlaceHolderFragment(){
-        }
-
-        @Nullable
-        @Override
-        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            List<String> elements = new ArrayList<>();
-            elements.add("Today - Sunny - 88/63");
-            elements.add("Monday - Sunny - 88/63");
-            elements.add("Thursday - Sunny - 88/63");
-            elements.add("Wednesday - Sunny - 88/63");
-            elements.add("Thursday - Sunny - 88/63");
-            elements.add("Friday - Sunny - 88/63");
-            elements.add("Saturday - Sunny - 88/63");
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                    rootView.getContext(),
-                    R.layout.listview_item_layout,
-                    R.id.listview_item_textview,
-                    elements);
-            ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(adapter);
-            return rootView;
         }
     }
 }
